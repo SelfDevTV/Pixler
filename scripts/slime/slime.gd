d5cf3da2a86d03ad3c2cb4a5334bd0adc16c8eac
SelfDevTV <selfdevtv@gmail.com>
2025-11-05 22:56:09 +0100

    added paint progress state and visuals

--- scripts/slime/slime.gd
+++ scripts/slime/slime.gd
@@ -2,6 +2,8 @@ class_name Slime
 extends Node2D
 
 var next_target: Vector2
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var progress_bar: ProgressBar = $ProgressBar
 
 @export var move_speed: float = 100.0
 @export var paint_speed: float = 1.5
--- scripts/state_painting.gd
+++ scripts/state_painting.gd
@@ -2,15 +2,23 @@ extends State
 
 @onready var paint_timer: Timer = $PaintTimer
 
+
 func enter() -> void:
     paint_timer.wait_time = slime.paint_speed
     paint_timer.start()
+    slime.animation_player.play("paint")
     print("cell pos for paint: ", GridManager.world_to_cell(slime.global_position))
+    slime.progress_bar.max_value = paint_timer.wait_time
+    slime.progress_bar.value = 0
+    
 
 func _on_paint_timer_timeout() -> void:
+    slime.animation_player.stop()
     paint_timer.stop()
-     
+    
+func update(delta: float) -> void:
+    slime.progress_bar.value = paint_timer.wait_time - paint_timer.time_left
+    print(paint_timer.time_left)
+    print("test")
 
-func update(delta: float):
-    # TODO: update painting progress on that cell
-    pass
+    
